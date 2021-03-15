142086ae3a3480cf5f231ca057b6b7d408dda438
Francisco Garcia <garciafran@hotmail.com>
2021-03-15 13:37:30 -0500

    Fundamentos de Flask

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, make_response, redirect
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+    user_ip = request.remote_addr
+    
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
+    return 'Hello work flask en platzi, tu IP es{}, usando cookies'.format(user_ip)
+
+
